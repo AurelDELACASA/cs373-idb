faad5e5e7f06cc8b8c5300d32637e2a0f5043712
Caelan Evans <evans.caelan@gmail.com>
2017-03-23 14:22:22 -0500

    Added documentation for app.py

--- app/app.py
+++ app/app.py
@@ -8,10 +8,16 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def splash(path):
+    """
+    Route for generic path
+    """
     return make_response(open(os.path.join(os.path.dirname(__file__), "templates/index.html")).read())
 
 @app.route('/api/tournaments', methods=['GET'])
 def return_tournaments():
+    """
+    API route for Tournaments
+    """
     t1 = Tournament("name1", "01/02/03", "California", 195, "/static/images/bracketIcon.png")
     t2 = Tournament("name2", "04/05/06", "Texas", 30, "/some/tpath2")
     t3 = Tournament("name3", "07/08/09", "New York", 195, "/some/tpath3")
@@ -21,11 +27,17 @@ def return_tournaments():
 
 @app.route('/api/tournament/<name>', methods=['GET'])
 def return_tournament(name):
+    """
+    API route for individual Tournament
+    """
     t1 = Tournament("name1", "01/02/03", "California", 195, "/static/images/bracketIcon.png")
     return jsonify(tournament = t1.__dict__)
 
 @app.route('/api/participants', methods=['GET'])
 def return_participants():
+    """
+    API route for Participants
+    """
     p1 = Participant("Mang0", "/static/images/mango.png", "Joseph", "Mario", "Canada")
     p2 = Participant("Friberg", "/some/ppath2", "Dave", "Samus", "Ohio")
     p3 = Participant("summit1g", "/some/ppath3", "Steve", "Link", "Florida")
@@ -35,11 +47,17 @@ def return_participants():
 
 @app.route('/api/participant/<name>', methods=['GET'])
 def return_participant(name):
+    """
+    API route for individual Participant
+    """
     p1 = Participant("Mang0", "/static/images/mango.png", "Joseph", "Mario", "Canada")
     return jsonify(participant = p1.__dict__)
 
 @app.route('/api/characters', methods=['GET'])
 def return_characters():
+    """
+    API route for Characters
+    """
     c1 = Character("Mario", "Super Mario Borthers", 100, ["Dunk", "Back Throw", "Cape"], 1999)
     c2 = Character("Link", "The Legend of Zelda", 104, ["Hook Shot", "Boomerang", "Bombs"], 1986)
     c3 = Character("Samus", "Metroid", 110, ["Energy Ball", "Missiles", "Screw Attack"], 1986)
@@ -49,7 +67,10 @@ def return_characters():
 
 @app.route('/api/character/<name>', methods=['GET'])
 def return_character(name):
-    c1 = Character("Mario", "super Mario Borthers", 100, ["Dunk", "Back Throw", "Cape"], 1999)
+    """
+    API route for individual Character
+    """
+    c1 = Character("Mario", "Super Mario Borthers", 100, ["Dunk", "Back Throw", "Cape"], 1999)
     return jsonify(character = c1.__dict__)
 
 
