3d80797183738dc1529ac70ebb509d66dd7f5ac4
Caelan Evans <evans.caelan@gmail.com>
2017-04-03 23:32:19 -0500

    Convert dictionaries to tuples

---
+++ data/construct_characters.py
@@ -0,0 +1,23 @@
+import pickle
+
+if __name__ == "__main__":
+    movetypes = list(dict(pickle.load(open("movetypes.pickle", "rb"))).items())[0][1]
+
+    movenames = dict(pickle.load(open("movenames.pickle", "rb")))
+    universes = dict(pickle.load(open("universes.pickle", "rb")))
+    tiers = dict(pickle.load(open("tiers.pickle", "rb")))
+
+    characters = list()
+
+    for key in movenames.keys():
+        name = key[:len(key) - 1].replace("%26", "&").replace(".", "").replace("_", " ")
+        universe = universes[key]
+        tier = tiers[key]
+        movelist = ", ".join(set([x for x in movenames[key] if x != ' ']))
+        filename = key.lower()[:len(key) - 1].replace(".", "") + ".png"
+
+        characters.append(tuple([str(name), str(universe), str(tier), str(movelist), str(filename)]))
+
+    print(characters)
+    pickle.dump(characters, open("characters.pickle", "wb"))
+    print("Done.")
